499d06ac6e671222e801b0feaf97d36abf9ec9ba
lidiarodriguezlab-sketch <lidiarodriguezlab@gmail.com>
2026-08-05 14:19:11 +0100

    Update GitHub social link in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
                 },
             },
             
-            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/lidiarodriguezlab-sketch/Documentacion_Femete.git' }],
             customCss: ['./src/Style/custom.css'],
             
             // Reemplaza el espacio de búsqueda por tu contenedor doble
